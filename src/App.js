0cda6e5efdb818263bca63b403b16d88e77d8688
jdjason569569 <jdjason569@gmail.com>
2023-03-23 21:47:35 -0500

    menu animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import './vars.css';
 
 import { MyRoutes } from './routes/routes';
 
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,8 @@
 import { useNavigate } from "react-router-dom"
 import { auth } from "../../firebase"
+import '../home/home.css';
+import logo from '../../assets/yesSr.png';
+
 
 
 
@@ -11,11 +14,51 @@ export default function Home({ name }) {
         return auth.signOut();
     }
 
-
     return (
         <>
-            <h2>{name ? `Bienvenido - ${name}` : "Inicia session"}</h2>
-            {name && <button onClick={exit}>Salir</button>}
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                rel="stylesheet" />
+
+            <div class="menu">
+                <input hidden type="checkbox" id="toggle" />
+                <label id="show-menu" for="toggle">
+                    <div class="_btn">
+                        <img className="image menu_btn" src={logo} />
+                    </div>
+                    <div className="_btn">
+                        <i class="material-icons md-36">photo_camera</i>
+                    </div>
+                    <div className="_btn">
+                        <i class="material-icons md-36">photo</i>
+                    </div>
+                    <div className="_btn">
+                        <i class="material-icons md-36">music_note</i>
+                    </div>
+                    <div className="_btn">
+                        <i class="material-icons md-36">chat_bubble</i>
+                    </div>
+                    <div className="_btn">
+                        <i class="material-icons md-36">settings</i>
+                    </div>
+                    <div className="_btn">
+                        <i class="material-icons md-36">phone</i>
+                    </div>
+                    <div className="_btn">
+                        <i class="material-icons md-36">settings</i>
+                    </div>
+                    <div className="_btn">
+                        <i class="material-icons md-36">phone</i>
+                    </div>
+                </label>
+            </div>
+            <div className="container-home">
+                <div>
+                    <h2>{name ? `Bienvenido  ${name}` : "Inicia session"}</h2>
+                </div>
+                <div>
+                    {name && <button className="btn btn-light btn-sm rounded btn-style" onClick={exit}>Salir</button>}
+                </div>
+            </div>
         </>
     )
 }
