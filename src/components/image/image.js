5d59200d7cd4086486079132d14c0c3eb5a4aafe
jdjason569569 <jdjason569@gmail.com>
2023-04-28 21:44:22 -0500

    add images

--- src/components/image/image.js
+++ src/components/image/image.js
@@ -1,27 +1,67 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { auth, storage } from '../../firebase';
-import {ref, uploadBytes} from 'firebase/storage';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import './image.css';
+import { ToastContainer, toast } from 'react-toastify';
 
-export default function Image(){
+export default function Image() {
+  const [idFirebaseUser, setIdFirebaseUser] = useState(null);
+  const [apiUrl, setApiUrl] = useState(process.env.REACT_APP_API);
 
 
-    useEffect(() => { 
-    }, )
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setIdFirebaseUser(user.uid);
+      } else {
+        setIdFirebaseUser(null);
+      }
+    });
+  }, []);
 
-    const uploadImage = (file) =>{
-        const storageRef = ref(storage, 'hola') ;
-        uploadBytes(storageRef, file).then(value =>{
-            console.log(value);
-        })
+  const uploadImage = async (file) => {
+    const date = new Date();
+    const name = `${idFirebaseUser.slice(0, 5)}-${date.getMilliseconds().toString()}`
+    const storageRef = ref(storage, name);
+    await uploadBytes(storageRef, file);
+    const url = await getDownloadURL(storageRef);
+    const responseTask = await saveImageByUser(url);
+    toast.success('Agregaste una imagen', { autoClose: 1000 }, { position: toast.POSITION.TOP_CENTER });
+  }
 
+  /**
+   * Allow return an user by firebase code
+   */
+  const getUserById = async () => {
+    const respGetUserById = await fetch(`${apiUrl}/user/${idFirebaseUser}`);
+    const { id_users } = await respGetUserById.json();
+    return id_users;
+  }
+
+
+  /**
+   * Allow save task by user
+   */
+  const saveImageByUser = async (url) => {
+    const idUser = await getUserById();
+    const image = {
+      id_users : idUser,
+      name : url
     }
-    
-
-
-     return (
-        <div className='container-image'>
-        <input type='file' onChange={e => uploadImage(e.target.files[0])}></input>
-        </div>
-     )
+        
+    const responseAddImage = await fetch(`${apiUrl}/images`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(image)
+    });
+    return await responseAddImage.json();
+  }
+  return (
+    <div className='container-image'>
+      <ToastContainer />
+      <input type='file' onChange={e => uploadImage(e.target.files[0])}></input>
+    </div>
+  )
 }
